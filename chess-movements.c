ac3f9f246a938067b76d7b45d28ccde099dfb8ab
Pedro Henrique Candido Nascimento <91481238+PH-CN@users.noreply.github.com>
2025-12-09 20:45:45 +0000

    desafio mestre

--- chess-movements.c
+++ chess-movements.c
@@ -1,32 +1,49 @@
 #include <stdio.h>
 
-int main() {
-    // Movimentação da torre
-    int t = 1, b = 1, r = 1, c = 1;
-    do {
+int t = 1, b = 1, r = 1, c = 1;;
+
+void moverTorre(int casas) {
+    if (casas > 0) {
         printf("A torre se moveu para a direita!\n");
         printf("Casa atual: %d\n", t);
         t++;
-    } while (t <= 5);
+        moverTorre(casas - 1);
+    }
+}
+
+void moverBispo(int casas) {
+    if (casas > 0) {
+        printf("O  bispo se moveu para cima, direita!\n");
+        printf("Casa atual: %d\n", b);
+        b++;
+        moverBispo(casas - 1);
+    }
+}
+
+void moverRainha(int casas) {
+    if (casas > 0) {
+        printf("A rainha se moveu para esquerda!\n");
+        printf("Casa atual: %d\n", r);
+        r++;
+        moverRainha(casas - 1);
+    }
+}
+
+int main() {
+    // Movimentação da torre
+    moverTorre(5);
 
     // Movimentação do bispo
-    while (b <= 5) {
-        printf("O bispo se moveu para cima, direita\n");
-        printf("Casa atual %d\n", b);
-        b++;
-    }
+    moverBispo(5);
 
     // Movimentação da rainha
-    for (r; r <= 8; r++) {
-        printf("A rainha se moveu para a esquerda\n");
-        printf("Casa atual %d\n", r);
-    }
+    moverRainha(8);
     // Movimentação do cavalo
-    while (c <= 1) {
-        for (int i = 1; i <= 2; i++) {
-            printf("O cavalo andou para baixo\n");
+    for (int i = 0, j = 1; i < 3 && j < 4; i++, j++) {
+        if (i == 2 && j == 3) {
+            printf("O cavalo andou para a direita!\n");
+            break;
         }
-        printf("O cavalo andou para a esquerda\n");
-        c++;
+        printf("O cavalo andou para cima!\n");
     }
 }
