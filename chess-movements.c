384f420672a94baf0ef02f30ebf61bbe0e14c66d
Pedro Henrique Candido Nascimento <91481238+PH-CN@users.noreply.github.com>
2025-12-09 19:48:15 +0000

    desafio aventureiro

--- chess-movements.c
+++ chess-movements.c
@@ -2,7 +2,7 @@
 
 int main() {
     // Movimentação da torre
-    int t = 1, b = 1, r = 1;
+    int t = 1, b = 1, r = 1, c = 1;
     do {
         printf("A torre se moveu para a direita!\n");
         printf("Casa atual: %d\n", t);
@@ -20,5 +20,13 @@ int main() {
     for (r; r <= 8; r++) {
         printf("A rainha se moveu para a esquerda\n");
         printf("Casa atual %d\n", r);
+    }
+    // Movimentação do cavalo
+    while (c <= 1) {
+        for (int i = 1; i <= 2; i++) {
+            printf("O cavalo andou para baixo\n");
+        }
+        printf("O cavalo andou para a esquerda\n");
+        c++;
     }
 }
